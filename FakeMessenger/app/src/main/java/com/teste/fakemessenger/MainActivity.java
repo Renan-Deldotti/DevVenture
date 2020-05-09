b610da5a913a038882de21c85b5c5c40ade4b347
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-09 12:10:46 -0300

    Update 09-05

--- FakeMessenger/app/src/main/java/com/teste/fakemessenger/MainActivity.java
+++ FakeMessenger/app/src/main/java/com/teste/fakemessenger/MainActivity.java
@@ -1,18 +1,53 @@
 package com.teste.fakemessenger;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ID = MainActivity.class.getSimpleName() + " Message";
+    public static final int REQUEST_ANSWER = 1;
+
+    private EditText editTextMessage;
+    private TextView textViewAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextMessage = findViewById(R.id.editText_message);
+        textViewAnswer = findViewById(R.id.textView_answer);
     }
 
     public void callNewActivity(View view) {
+        Intent intent = new Intent (MainActivity.this,NewActivity.class);
+        intent.putExtra(EXTRA_ID,editTextMessage.getText().toString().trim());
+        startActivity(intent);
+    }
+
+    public void callNewActivityForResult(View view) {
+        Intent intent = new Intent (MainActivity.this,NewActivity.class);
+        intent.putExtra(EXTRA_ID,editTextMessage.getText().toString().trim());
+        startActivityForResult(intent,REQUEST_ANSWER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_ANSWER){
+            if (resultCode == RESULT_OK){
+                if (data!=null && data.hasExtra(NewActivity.ANSWER_ID)){
+                    textViewAnswer.setText(data.getStringExtra(NewActivity.ANSWER_ID));
+                }
+            }
+        }
     }
 }
--- FakeMessenger/app/src/main/java/com/teste/fakemessenger/NewActivity.java
+++ FakeMessenger/app/src/main/java/com/teste/fakemessenger/NewActivity.java
@@ -2,13 +2,40 @@ package com.teste.fakemessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NewActivity extends AppCompatActivity {
+
+    public static final String ANSWER_ID = NewActivity.class.getSimpleName() + " ANSWER";
+
+    private TextView textViewMessageReceived;
+    private EditText editTextAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+
+        textViewMessageReceived = findViewById(R.id.textView_message);
+        editTextAnswer = findViewById(R.id.editTextAnswer);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(MainActivity.EXTRA_ID)){
+            textViewMessageReceived.setText(intent.getStringExtra(MainActivity.EXTRA_ID));
+        }else{
+            textViewMessageReceived.setText("Não há menssagens");
+        }
+    }
+
+    public void sendAnswer(View view) {
+        String answer = editTextAnswer.getText().toString().trim();
+        Intent answerIntent = new Intent();
+        answerIntent.putExtra(ANSWER_ID,answer);
+        setResult(RESULT_OK,answerIntent);
+        finish();
     }
 }
