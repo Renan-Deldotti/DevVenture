c546deba8a2e7ef0c6a1d03dc872b21cc5c9f589
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-12 22:14:21 -0300

    Update

--- ConstraintLayoutLab/app/src/main/java/com/teste/constraintlayoutlab/MainActivity.java
+++ ConstraintLayoutLab/app/src/main/java/com/teste/constraintlayoutlab/MainActivity.java
@@ -3,6 +3,15 @@ package com.teste.constraintlayoutlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup_gender);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
