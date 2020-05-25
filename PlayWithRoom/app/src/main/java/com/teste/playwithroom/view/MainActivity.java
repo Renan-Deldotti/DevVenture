f01b2ec2ac39727e06d70bf8d43b15808abfc37b
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-24 22:16:08 -0300

    Update MainActivity.java

--- PlayWithRoom/app/src/main/java/com/teste/playwithroom/view/MainActivity.java
+++ PlayWithRoom/app/src/main/java/com/teste/playwithroom/view/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     private PlayGamesAdapter adapter;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
